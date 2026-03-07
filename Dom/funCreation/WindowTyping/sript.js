9309df85361c9d6e7fbd3f8a9e3c8de64d7a0fd8
TirthbasMishra <mishratirthabas17@gmail.com>
2026-03-08 02:05:38 +0530

    jsDom

--- Dom/funCreation/WindowTyping/sript.js
+++ Dom/funCreation/WindowTyping/sript.js
@@ -1,9 +1,28 @@
-let win = document.querySelector("h1");
-window.addEventListener("keydown",function(dets){
-   if(dets.key===" "){
-      win.textContent="SPC";
-   }else{
-      win.textContent=dets.key;
+// let win = document.querySelector("h1");
+// window.addEventListener("keydown",function(dets){
+//    if(dets.key===" "){
+//       win.textContent="SPC";
+//    }else{
+//       win.textContent=dets.key;
 
-   }
-})
+//    }
+// })
+
+let btn = document.querySelector("#btn");
+let inpId = document.querySelector("#fileInp")
+btn.addEventListener("click", (t) => {
+   inpId.click();
+   console.log(t)
+   
+});
+
+inpId.addEventListener("change", (e) => {
+      console.log(e)
+      let file = e.target.files[0];
+      // console.log(file.name);
+      if(file){
+         btn.textContent = file.name;
+      }else{
+         console.log("file not exist")
+      }
+   });
